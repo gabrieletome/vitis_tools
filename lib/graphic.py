1601716d7d12ecad9c000ef3a4d56bfa63a358e8
gabrieletome <39188419+gabrieletome@users.noreply.github.com>
2021-02-26 15:03:22 +0100

    print only one json

--- lib/graphic.py
+++ lib/graphic.py
@@ -180,8 +180,10 @@ def drawGraph(net, namefile, pearson, autoSaveImg, list_Genes, range_frel, typeP
         plt.show()
 
     #Create json file of graph
-    with open(namefile+'.json', 'w', encoding='utf-8') as f:
-        json.dump(json_graph.node_link_data(G), f, ensure_ascii=False, indent=4)
+    if typePrint:
+        print('Create: \''+namefile+'.json\'', flush=True)
+        with open(namefile+'.json', 'w', encoding='utf-8') as f:
+            json.dump(json_graph.node_link_data(G), f, ensure_ascii=False, indent=4)
     #Clean graph and pyplot
     G.clear()
     plt.clf()
