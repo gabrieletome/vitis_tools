601099dcdd0ab27ebfa5ebc04471f921fdd707bd
Gabriele Tomè <39188419+gabrieletome@users.noreply.github.com>
2021-07-29 17:13:10 +0200

    fix permission for R packages

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #install python libraries
 pip3 install -r import_doc/requirements.txt
 #install BiocManager and topGO
-R -e "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager'); library('BiocManager'); BiocManager::install('topGO')"
+sudo R -e "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager'); library('BiocManager'); BiocManager::install('topGO')"
